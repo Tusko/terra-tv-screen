f95c8228f4b802d167b28c5e92eedce942f83770
Tusko <tusko@photoinside.me>
2020-09-04 10:15:14 +0300

    plan

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,38 @@
 <template>
   <div id="app">
-    <currentTime />
-    <weather />
+    <!-- <currentTime />
+    <weather />-->
+    <h2>План конференції</h2>
+    <p>
+      1. Наші спільні здобутки та напрацювання з розвитку компанії.
+      <b>- Volodymyr Nerubenko</b>
+    </p>
+    <p>
+      2. Оновлена концепція продуктів TerraLab. Базова, Стандартна, Індивідуальна конфігурації.
+      <b>- Svitlana Sytnik</b>
+    </p>
+
+    <h3>Систематизація роботи проектного відділу.</h3>
+    <p>
+      3. Проектний Flow. Статут проекту. Ролі та функції. -
+      <b>Krystyna Serbinova</b>
+    </p>
+    <p>
+      4. Методологія навчання. -
+      <b>Krystyna Serbinova</b>
+    </p>
+
+    <h3>Проекти COVID-19</h3>
+    <p>
+      5. TerraLab допомагає у боротьбі з коронавірусом.
+      <b>- Volodymyr Nerubenko</b>
+    </p>
+    <p>
+      6. Презентація функціоналу COVID-19.
+      <b>Valentyna Lemish</b>
+    </p>
+
+    <h4>Загальне обговорення розвитку компанії. Обмін думками, факапами та ідеями.</h4>
   </div>
 </template>
 
@@ -9,8 +40,8 @@
 export default {
   name: "App",
   components: {
-    currentTime: () => import("./components/currentTime"),
-    weather: () => import("./components/weather")
-  }
+    // currentTime: () => import("./components/currentTime"),
+    // weather: () => import("./components/weather")
+  },
 };
 </script>
